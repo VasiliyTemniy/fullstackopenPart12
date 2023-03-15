da7c3137dc553addfcbb90cf15b2ffd082985cfd
VasiliyTemniy <flash_er@mail.ru>
2023-03-15 21:43:02 +0300

    mongo + docker = good

--- my-app/bloglist-backend/utils/config.js
+++ my-app/bloglist-backend/utils/config.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const PORT = process.env.PORT
 
 const MONGODB_URI =
-  process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
+  process.env.NODE_ENV === 'development' ? process.env.DEV_MONGO_URL : process.env.MONGODB_URI
 
 const SECRET = process.env.SECRET
 
